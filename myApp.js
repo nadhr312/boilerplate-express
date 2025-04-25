81da01639e8d750d3aa92a5cc8faedfdd4f64be4
nadhr312 <nadhirafgh@gmail.com>
2025-04-25 13:48:09 +0700

    Add new app

--- myApp.js
+++ myApp.js
@@ -4,8 +4,13 @@ let app = express();
 console.log("Hello World");
 
 
-app.get("/", function(req,res){
+/*app.get("/", function(req,res){
     res.send("Hello Express");
+})*/
+
+app.get("/", function(req,res){
+    const absolutePath = __dirname + '/views/index.html'
+    res.sendFile(absolutePath)
 })
 
 
