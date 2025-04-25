3cbb11ba9cee3cbe4fc3d8aa6210d3c95f6f6ee3
nadhr312 <nadhirafgh@gmail.com>
2025-04-25 13:42:12 +0700

    New app

--- myApp.js
+++ myApp.js
@@ -1,10 +1,12 @@
 let express = require('express');
 let app = express();
 
-console.log("Hello World")
+console.log("Hello World");
 
 
-
+app.get("/", function(req,res){
+    res.send("HelloExpress");
+})
 
 
 
